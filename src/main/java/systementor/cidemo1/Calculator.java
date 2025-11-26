12e8a0bf62311a3b8328ef66c4ed45d2a980216f
alialsaade96 <alialsaade@live.com>
2025-11-26 16:22:59 +0100

    changes

--- src/main/java/systementor/cidemo1/Calculator.java
+++ src/main/java/systementor/cidemo1/Calculator.java
@@ -1,24 +1,6 @@
-package systementor.cidemo1;
+public int add(int a, int b) { return a + b; }
+public int subtract(int a, int b) { return a - b; }
+public int multiply(int a, int b) { return a * b; }
+public int divide(int a, int b) { return a / b; }
 
-public class Calculator {
-
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    public int subtract(int a, int b) {
-        return a - b;   // Viktigt: korrekt subtraktion
-    }
-
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-
-    public int divide(int a, int b) {
-        if (b == 0) {
-            throw new IllegalArgumentException("Division by zero not allowed");
-        }
-        return a / b;
-    }
-}
 
